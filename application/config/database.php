0389def7e290d269ca269923480e0627cf5cad9b
Chris Galten <schplatjr@gmail.com>
2011-11-09 15:12:48 -0700

    updating the connection

--- application/config/database.php
+++ application/config/database.php
@@ -19,6 +19,7 @@ if(isset($_SERVER['HTTP_HOST']))
     $config['base_url'] = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
     $config['base_url'] .= '://'. $_SERVER['HTTP_HOST'];
     $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+	echo $config['base_url'];
 }
 
 else
